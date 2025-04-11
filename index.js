0991230617f71842ad8d3a9968ac4bba4f05b881
Anju Thomas <anjunoblethomas@gmail.com>
2025-04-11 15:04:41 -0400

    Add login API

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const dotenv = require('dotenv');
 const express = require('express');
 const bcrypt = require('bcrypt');
 const models = require('./models');
+const jwt = require('jsonwebtoken');
+const {expressjwt } = require('express-jwt');
 
 dotenv.config(); // loading environment variables from .env file
+console.log('JWT_SECRET:', process.env.JWT_SECRET);
 const app = express();
 
 app.use(express.json()); // this is a middleware to parse incoming JSON requests
@@ -11,6 +14,13 @@ app.use(express.json()); // this is a middleware to parse incoming JSON requests
 app.get('/', (req, res) => {
     res.send('Hello World!!');
 });
+
+app.use(
+    expressjwt({
+      secret: process.env.JWT_SECRET,
+      algorithms: ["HS256"],
+    }).unless({ path: ["/login", "/signup", "/"] }) // skip protection on login/signup
+);
 
 // signup route POST
 app.post('/signup', async (req, res) => {
@@ -57,14 +67,59 @@ app.post('/signup', async (req, res) => {
     }
 });
 
+// LOGIN post route
+app.post('/login', async (req, res, next) => {
+    const {email, password } = req.body; // pulling email and password from request body
+    const dbModels = await models.default; // loading sequelize models (ORM Objects) that let us interact with the database
+    try{
+
+        const findUser = await dbModels.User.findOne({
+            attributes: ['password', 'name', 'id'], // only selecting password, name, and id
+            where: { email } // looking for a user with a given email
+        });
+
+        if(!findUser){
+            res.status(400);
+            return res.json({message: "Invalid email or password", "sys_message": "invalid_email_password"});
+        }
+
+        // now comparing the hashed password in the DB with the user entered one
+        const matched = await bcrypt.compare(password, findUser.password);
+
+        if (matched) {
+            const token = jwt.sign(
+            { user_id: findUser.id },
+            process.env.JWT_SECRET,
+            { algorithm: "HS256", expiresIn: "1h" }
+            );
+        
+            return res.json({
+                message: "Login successful",
+                sys_message: "login_success",
+                token,
+                name: findUser.name
+            });
+        }
+
+        res.status(403);
+        res.json({message: "invalid email or password", sys_message: "login_failed" })
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message: 'An error occured.', error: error.message})
+    }
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
+        console.log(`listening on ${PORT}!!`)
+});
+
 //app.post('/', (req, res))
 //app.patch('/', ())
 //app.delete()
 
 // PORT
 
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`listening on ${PORT}!!`)
-});
