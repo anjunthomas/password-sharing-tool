803589d8cbb6b862c6b8fc7a6109e32c1840c815
Anju Thomas <anjunoblethomas@gmail.com>
2025-04-28 15:11:53 -0400

    changed routing from button to tabs, further styling needed

--- frontend/password-sharing-manager/src/pages/Home.jsx
+++ frontend/password-sharing-manager/src/pages/Home.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 // src/pages/Home.jsx
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   const navigate = useNavigate();
 
   return (
     
-    <div className="home-buttons">
-        <button onClick={() => navigate("/login")}>Login</button>
-        <button onClick={() => navigate("/signup")}>Signup</button>
- 
+    <div className="tabs">
+        <Link to="/signup">SignUp</Link>
+        <Link to="/login">Login</Link>
     </div>
   );
 }
