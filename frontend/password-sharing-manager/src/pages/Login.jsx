ad8b4734a030c0685fb7835f1e4e6aa0de39dc1d
Anju Thomas <anjunoblethomas@gmail.com>
2025-04-28 14:57:12 -0400

    Added input fields to login and signup pages

--- frontend/password-sharing-manager/src/pages/Login.jsx
+++ frontend/password-sharing-manager/src/pages/Login.jsx
@@ -3,11 +3,37 @@ import FormInput from "../components/FormInput";
 
 export default function Login(){
 
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    })
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // stopping the page from refreshing with each change
+        console.log("Login form submitted:", formData);
+    }
     return (
-        <div className="login-wrapper">
-            <h3 className="login">
-                Login Page
-            </h3>
+        <div className="form-wrapper">
+            <h2> Login </h2>
+            <form onSubmit={handleSubmit} className="form-container">
+                <label>Email</label>
+                <input
+                    email="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder="Email"/>
+
+                <label>Password</label>
+                <input
+                    password="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    placeholder="Password"/>
+            </form>
         </div>
     );
 }
--- frontend/password-sharing-manager/src/pages/Signup.jsx
+++ frontend/password-sharing-manager/src/pages/Signup.jsx
@@ -1,11 +1,60 @@
 import React, { useState } from "react";
 
 export default function Signup(){
+
+    const [formData, setFormData] = useState({
+        userName: "",
+        email: "",
+        password: "",
+        encryptionKey: ""
+    });
+
+    // every input typed into an input box calls this function
+    // e.target.name is the name of the input: email, password, name
+    // e.target.value is the new value that was typed
+    // setFormData - copies the existing form data, updates what was changed, rpaces it in the form dat state
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault(); // stops the page from refreshing which is default HTML behavior
+        console.log("Signup form submitted:", formData);
+        // Here you would call your backend API
+    };
+
     return (
-        <div>
-            <h3>
-                Signup!
-            </h3>
+        <div className="form-wrapper">
+            <h2> Sign Up</h2>
+            <form onSubmit={handleSubmit} className="form-container">
+                <label>Name</label>
+                <input 
+                    name="userName" 
+                    value={formData.userName}
+                    onChange={handleChange}
+                    placeholder="Name" />    
+
+                <label>Email</label>   
+                <input
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder="email"/>
+                
+                <label>Password</label>
+                <input
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    placeholder="password"/>
+
+                <label>Encryption Key</label>
+                <input
+                    name="encryptionKey"
+                    value={formData.encryptionKey}
+                    onChange={handleChange}
+                    placeholder="encryptionKey"/>
+            </form>  
         </div>
     )
 }
