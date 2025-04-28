f18f53ea80af9a75197c91f0407ba2a6bb2213bf
Anju Thomas <anjunoblethomas@gmail.com>
2025-04-28 13:16:29 -0400

    initialized a react frontend for the password-sharing-app

---
+++ frontend/password-sharing-manager/src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from 'react'
+import './styles/App.css'
+
+export default function App() {
+
+  return (
+      <Router>
+        
+      </Router>
+  );
+}
+
+
---
+++ frontend/password-sharing-manager/src/components/FormInput.jsx
@@ -0,0 +1,9 @@
+
+const FormInput = (props) => {
+    return (
+        <div className="formInput">
+            
+
+        </div>
+    )
+}
